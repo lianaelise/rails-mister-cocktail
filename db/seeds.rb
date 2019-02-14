4bda6ab0133d3b3145589dd4dc3d28731c73cfc1
liana griffin <lianaelise@hotmail.com>
2019-02-14 17:07:28 +0200

    herko

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-Ingredient.create(name: 'champagne')
-Ingredient.create(name: 'thyme')
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |i|
+  new_ingredient = Ingredient.create(name: i["strIngredient1"].to_s)
+end
