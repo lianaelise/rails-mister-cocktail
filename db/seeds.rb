399676f9e760ccc22d43b3b3ee9fabda9710aae2
liana griffin <lianaelise@hotmail.com>
2019-02-14 18:09:45 +0200

    adding a few more ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,21 @@ ingredients["drinks"].each do |i|
   new_ingredient = Ingredient.create(name: i["strIngredient1"].to_s)
 end
 
+Ingredient.create(name:“grapefruit peel”)
+Ingredient.create(name:“lemon peel”)
+Ingredient.create(name:“orange peel”)
+Ingredient.create(name:“rosemary”)
+Ingredient.create(name:“thyme”)
+Ingredient.create(name:“basil”)
+Ingredient.create(name:“cilantro”)
+Ingredient.create(name:“mint”)
+Ingredient.create(name:“grapefruit soda”)
+Ingredient.create(name:“arak”)
+Ingredient.create(name:“aperol”)
+Ingredient.create(name:“salt”)
+Ingredient.create(name:“honey”)
+Ingredient.create(name:“cachaca”)
+Ingredient.create(name:“chambord”)
+Ingredient.create(name:“amarena cherries”)
+Ingredient.create(name:“rosemary”)
+
