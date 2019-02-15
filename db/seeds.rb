9856f26ff793c5d80d8f2745338504da925caa72
liana griffin <lianaelise@hotmail.com>
2019-02-15 21:48:04 +0200

    fri update navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Review.destroy_all
-Cocktail.destroy_all
-Dose.destroy_all
-Ingredient.destroy_all
-
-
-require 'json'
-require 'open-uri'
+# require 'json'
+# require 'open-uri'
 
 
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
 
-ingredients["drinks"].each do |i|
-  new_ingredient = Ingredient.create(name: i["strIngredient1"].to_s)
-end
+# ingredients["drinks"].each do |i|
+#   new_ingredient = Ingredient.create(name: i["strIngredient1"].to_s)
+# end
 
 Ingredient.create(name:“grapefruit peel”)
 Ingredient.create(name:“lemon peel”)
