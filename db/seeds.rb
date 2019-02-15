f255091839c79cfd9587e859563934831b87035f
liana griffin <lianaelise@hotmail.com>
2019-02-15 22:16:02 +0200

    2.15. adding ingredidnets

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,6 @@
 # require 'json'
 # require 'open-uri'
 
-
-
 # url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 # ingredients_serialized = open(url).read
 # ingredients = JSON.parse(ingredients_serialized)
@@ -14,21 +12,20 @@
 #   new_ingredient = Ingredient.create(name: i["strIngredient1"].to_s)
 # end
 
-Ingredient.create(name:“grapefruit peel”)
-Ingredient.create(name:“lemon peel”)
-Ingredient.create(name:“orange peel”)
-Ingredient.create(name:“rosemary”)
-Ingredient.create(name:“thyme”)
-Ingredient.create(name:“basil”)
-Ingredient.create(name:“cilantro”)
-Ingredient.create(name:“mint”)
-Ingredient.create(name:“grapefruit soda”)
-Ingredient.create(name:“arak”)
-Ingredient.create(name:“aperol”)
-Ingredient.create(name:“salt”)
-Ingredient.create(name:“honey”)
-Ingredient.create(name:“cachaca”)
-Ingredient.create(name:“chambord”)
-Ingredient.create(name:“amarena cherries”)
-Ingredient.create(name:“rosemary”)
-
+Ingredient.create(name: 'grapefruit peel')
+Ingredient.create(name: 'lemon peel')
+Ingredient.create(name: 'orange peel')
+Ingredient.create(name: 'rosemary')
+Ingredient.create(name: 'thyme')
+Ingredient.create(name: 'basil')
+Ingredient.create(name: 'cilantro')
+Ingredient.create(name: 'mint')
+Ingredient.create(name: 'grapefruit soda')
+Ingredient.create(name: 'arak')
+Ingredient.create(name: 'aperol')
+Ingredient.create(name: 'salt')
+Ingredient.create(name: 'honey')
+Ingredient.create(name: 'cachaca')
+Ingredient.create(name: 'chambord')
+Ingredient.create(name: 'amarena cherries')
+Ingredient.create(name: 'rosemary')
